1981eb0c73fe82e228e32407fad54f7828ac918f
Bruno Feres <brunoferesvillela@icloud.com>
2022-01-19 17:51:38 -0300

    Added Meta Tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { Div } from 'atomize'
 import { Navbar } from '../components/Navbar'
 import { MainCard } from '../components/MainCard'
@@ -12,6 +13,16 @@ import { Footer } from '../components/Footer'
 const Home: React.FC = () => {
   return(
     <>
+      <Head>
+        <meta property="og:title" content="Dra. Beatriz Della" />
+        <meta property="og:type" content="business.business" />
+        <meta property="og:image" content="https://akns-images.eonline.com/eol_images/Entire_Site/2014212/rs_1024x759-140312104515-1024.Elsa-Frozen-Disney.jl.031214_copy.jpg" />
+        <meta property="og:url" content="https://www.drabeatrizdella.com" />
+        <meta name="twitter:card" content="https://akns-images.eonline.com/eol_images/Entire_Site/2014212/rs_1024x759-140312104515-1024.Elsa-Frozen-Disney.jl.031214_copy.jpg" />
+        <meta property="og:description" content="Serviços de Odonto" />
+        <meta property="og:site_name" content="Doutora Beatriz Della" />
+        <meta name="twitter:image:alt" content="Doutora Beatriz Della" />
+      </Head>
       <Navbar />
       <MainCard />
       <Services />
